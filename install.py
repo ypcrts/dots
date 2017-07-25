73ac615a2e5047d43d293b06f2c72673c5b83ff8
ypcrts <ypcrts@users.noreply.github.com>
2017-07-25 01:09:33 +0000

    wonderful

--- install.py
+++ install.py
@@ -5,6 +5,11 @@ from __future__ import print_function
 import sys
 import os
 import os.path
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f','--force', help="remove any existing file", action='store_true')
+opts = parser.parse_args()
 
 startdir = os.getcwd() 
 
@@ -17,7 +22,7 @@ with open('./MANIFEST', 'r') as fp:
     if len(parts) != 2:
       sys.stderr.write('skipping line {:d}: `{:s}`\n'.format(i, line))
 
-    dest, src = parts
+    dest, src = map(lambda p: os.path.expanduser(p), parts)
 
     srcabs = os.path.normpath(os.path.join(startdir, src))
     destabs = os.path.normpath(dest)
@@ -27,12 +32,14 @@ with open('./MANIFEST', 'r') as fp:
     if not os.path.isdir(destdir):
       os.makedirs(destdir, 0755)
 
+    if os.lstat(destabs):
+      if not args.force and os.stat(destabs):
+          print('skipping existing file:', destabs)
+          continue
+      os.remove(destabs) 
+
     os.chdir(destdir)
-    if os.path.isfile(destabs):
-      if os.path.islink(destabs):
-        continue
-      os.remove(destname) 
-    try: 
+    try:
       os.symlink(srcabs, destname)
     except OSError as e:
       print(e, '::', destabs)
