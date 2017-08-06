575cc91ff84836ea825e57f8a8ac81c336a61965
ypcrts <ypcrts@users.noreply.github.com>
2017-08-06 17:45:42 -0400

    linux by default

--- default/bash/os-aliases.bash
+++ default/bash/os-aliases.bash
@@ -1,4 +1,5 @@
 # vi: sts=2 ts=2 sw=2 et
+# linux by default -- these are GNU/linux things
 alias tree='tree -C'
 alias grep='grep --color=auto'   
 alias ls='ls --color=auto -hF --group-directories-first'
