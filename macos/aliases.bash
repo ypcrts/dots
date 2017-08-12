54ee09ade00203a593c2838a19f96a65206508f7
ypcrts <ypcrts@users.noreply.github.com>
2017-08-12 19:21:49 -0400

    remove sniff alias on macos - wtf y u exist

--- macos/aliases.bash
+++ macos/aliases.bash
@@ -1,8 +1,7 @@
 # vi: ft=sh sts=2 sw=2 sts=2 et
 alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-pidof () { ps -Acw | egrep -i $@ | awk '{print $1}'; }
-cdf() { cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"; }
+pidof () { ps -Acw | grep -iE $@ | awk '{print $1}'; } # this is kind of trash
+cdf() { cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"; }
 finder () {
   osascript 2>/dev/null <<EOL
   tell application "Finder"
