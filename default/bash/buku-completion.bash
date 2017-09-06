4d1071990e879f5d9eb28c9ffdea3efdd0470f61
ypcrts <ypcrts@users.noreply.github.com>
2017-09-06 14:15:42 -0400

    buku completion

---
+++ default/bash/buku-completion.bash
@@ -0,0 +1,84 @@
+#
+# Bash completion definition for buku.
+#
+# Author:
+#   Arun Prakash Jana <engineerarun@gmail.com>
+#
+
+_buku () {
+    COMPREPLY=()
+    local IFS=$' \n'
+    local cur=$2 prev=$3
+    local -a opts opts_with_args
+    opts=(
+        -a --add
+        --ai
+        -c --comment
+        --colors
+        -d --delete
+        --deep
+        -e --export
+        --expand
+        -f --format
+        -h --help
+        -i --import
+        --immutable
+        -j --json
+        -k --unlock
+        -l --lock
+        -m --merge
+        --nc
+        --np
+        -o --open
+        --oa
+        -p --print
+        -r --sreg
+        --replace
+        -s --sany
+        -S --sall
+        --shorten
+        --suggest
+        -t --stag
+        --tacit
+        --tag
+        --threads
+        --title
+        -u --update
+        --url
+        -V
+        -v --version
+        -w --write
+        -z --debug
+    )
+    opts_with_arg=(
+        -a --add
+        --colors
+        -e --export
+        --expand
+        -f --format
+        -i --import
+        --immutable
+        -m --merge
+        -r --sreg
+        --replace
+        -s --sany
+        -S --sall
+        --shorten
+        --threads
+        --url
+    )
+
+    # Do not complete non option names
+    [[ $cur == -* ]] || return 1
+
+    # Do not complete when the previous arg is an option expecting an argument
+    for opt in "${opts_with_arg[@]}"; do
+        [[ $opt == $prev ]] && return 1
+    done
+
+    # Complete option names
+    COMPREPLY=( $(compgen -W "${opts[*]}" -- "$cur") )
+    return 0
+}
+
+complete -F _buku buku
