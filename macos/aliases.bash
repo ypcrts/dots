8d249f1246e9e1dac354aaabebd7f85b086f01da
ypcrts <ypcrts@users.noreply.github.com>
2017-10-11 16:46:36 -0400

    pbcopy broken in tmux under High Sierra

--- macos/aliases.bash
+++ macos/aliases.bash
@@ -1,5 +1,9 @@
 # vi: ft=sh sts=2 sw=2 sts=2 et
 alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+alias pbcopy='/usr/local/bin/reattach-to-user-namespace pbcopy'
+alias pbpaste='/usr/local/bin/reattach-to-user-namespace pbpaste'
+alias pbp='pbpaste'
+alias pbc='pbcopy'
 pidof () { ps -Acw | grep -iE $@ | awk '{print $1}'; } # this is kind of trash
 cdf() { cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"; }
 finder () {
