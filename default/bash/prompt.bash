64ce15d4042ab192bd59238d3537d24e679b026a
ypcrts <32625281+ypcrts@users.noreply.github.com>
2017-11-15 20:06:57 -0500

    speed up bash prompt

--- default/bash/prompt.bash
+++ default/bash/prompt.bash
@@ -1,4 +1,5 @@
 # vi: ft=sh sts=2 ts=2 sw=2
+# set -uo pipefail
 bash_prompt_setup () {
   # local K='\033[0;30m' # black
   # local R='\033[0;31m' # red
@@ -12,53 +13,47 @@ bash_prompt_setup () {
 
   # param $1 str to hash
   __STRFARBE__ ()  {
-    (($# < 1)) && return 1
-    local HASH="$(echo \"$1\" | md5sum | sed -r 's/^([0-9a-f]*).*$/\U\1/')"
+    (($# != 1)) && return 1
+    local HEXY
+    HEXY="$(echo -n "$1" | md5sum | sed 's:^\([0-9a-f]*\).*$:\U\1:')"
 		if command -V bc >/dev/null 2>&1; then
-			echo "ibase=16;(${HASH}%5)+20" | bc
+      echo "ibase=16;((${HEXY}+2)%5)+20" | bc
 		else
-			python -c "import os;os.write(1,'{:d}'.format(int('${HASH}',16)%0x5+0x20))"
+			python -uEc "import os;os.write(1,'{:d}'.format(int('${HASH}',16)%0x5+0x20))"
+		fi
+  }
+  __INTFARBE__ ()  {
+    (($# != 1)) && return 1
+		if command -V bc >/dev/null 2>&1; then
+      echo "ibase=10;((${1}+1)%5)+32" | bc
+    else
+			python -uEc "import os;os.write(1,'{}'.format(${1}%0x5+0x20))"
 		fi
   }
 
-  local VIRTUALENV_TEIL=''
-  local NUTZER_TEIL=''
-  local FARBE=37
-  local HOST_FARBE=37
-  local BEGRENZER='✱'
-  local NUTZERZEIGEN=0
+  local FARBE BEGRENZER
 
-  if [[ "$SSH_TTY" ]]; then
+  if [[ "x$SSH_TTY" != 'x' ]]; then
     BEGRENZER='▞'
-    NUTZERZEIGEN=1
+  else
+    BEGRENZER=''
   fi
 
-  if ! ((__ZUHAUSE_PROMPT)); then
-    if ((EUID == 0)); then
-      FARBE=31
-      NUTZERZEIGEN=1
-    else
-      local NUTZER="$(id -un)"
-      case "$NUTZER" in
-        ypcrts)
-          ;;
-        *)
-          NUTZERZEIGEN=1
-          FARBE="$(__STRFARBE__ $NUTZER)"
-          ;;
-      esac
-    fi
+  if ((EUID == 0)); then
+    BEGRENZER="//"
+    FARBE=31
+  else
+    FARBE="$(__INTFARBE__ "$EUID")"
   fi
 
   local F="\[\e[0;${FARBE}m\]"
   local R='\[\e[0m\]'
 
-  if ((NUTZERZEIGEN)); then
-    HOST_FARBE="$(__STRFARBE__ `hostname`)"
-    NUTZER_TEIL="${F}\\u@\[\033[0;${HOST_FARBE}m\]\\H ${R}\[\033[0;\${RET_FARBE}m\]${BEGRENZER} "
-  fi
+  local HOST_FARBE="$(__STRFARBE__ "$HOSTNAME")"
 
-  PS1="\n${NUTZER_TEIL}${F}\\w \$(___VIRTUALENV_PROMPT__ 2>/dev/null)\[\033[0;34m\]\$(__git_ps1 \"(%s)\" 2>/dev/null)${R}\n\[\033[\${RET_FARBE}m\]\\$ ${R}"
+  local NUTZER_TEIL="${F}\\u\[\e[0;${HOST_FARBE}m\]@\\H${R} ${BEGRENZER} "
+
+  PS1="\n${NUTZER_TEIL}\[\033[\${RET_FARBE}m\]\\w${R} \$(___VIRTUALENV_PROMPT__ 2>/dev/null)\[\033[0;34m\]\$(__git_ps1 \"(%s)\" 2>/dev/null)${R}\n\[\033[\${RET_FARBE}m\]\\$ ${R}"
   PS2="${F}\ "
   PS4="${F}• "
 }
@@ -69,19 +64,19 @@ unset bash_prompt_setup
 ___VIRTUALENV_PROMPT__ () {
   [ -n "$RUHE" ] && return 0
   if [ -n "$VIRTUAL_ENV" ]; then
-    printf "\033[0;32m${VIRTUAL_ENV##*/}\033[0m "
+    echo -en "\033[0;32m${VIRTUAL_ENV##*/}\033[0m "
   fi
   if [ -n "$NVM_BIN" ]; then
-    printf "\033[0;35mn`nvm current | sed 's/v\(.\).*/\1/' 2>/dev/null`\033[0m "
+    echo -en "\033[0;35mn`nvm current | sed 's/v\(.\).*/\1/' 2>/dev/null`\033[0m "
   fi
   # TODO: Add rubygems (g), rvm (r)
 }
 
-function ___EXITSTAT () {
+___EXITSTAT () {
   local RET=$?
   if ((RET)); then
     RET_FARBE='0;31'
-    printf '\n\033[1;31m(%d)\033[0m ' "$RET"
+    echo -en "\n\033[1;31m("$RET")\033[0m "
   else
     RET_FARBE='0'
   fi
