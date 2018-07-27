9de4b765d2c409f20a0d7e5f760a47ef9c750615
ypcrts <32625281+ypcrts@users.noreply.github.com>
2018-07-27 12:35:12 -0400

    bspwm

---
+++ bspwm/screenlayout/bah.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3200x1800 --pos 1864x1440 --rotate normal --output DP-1-1 --mode 2560x1440 --pos 3320x0 --rotate normal --output DP-1-8 --mode 2560x1440 --pos 760x0 --rotate normal
