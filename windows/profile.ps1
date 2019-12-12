6bc725cf879ec6abb3278bccc406f900333f7435
ypcrts <32625281+ypcrts@users.noreply.github.com>
2019-12-12 14:25:05 -0500

    powershell: port ssh related aliases from bash (toll!)

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,9 +1,9 @@
 Set-Alias v vi
 Set-Alias vi vim
 Set-Alias vim nvim
-Function edit-powershell-profile {
-  vim $profile
-}
+#Function edit-powershell-profile {
+#  vim $profile
+#}
 Set-PSReadlineOption -EditMode vi
 
 Function bg() {
@@ -12,4 +12,21 @@ Function bg() {
 }
 Function firefoxp {
   Start-process firefox -ArgumentList @("-P", "-no-remote")
+}
+Function k {
+  start-service ssh-agent
+  ssh-add -l
+  if (!$?) {
+    ssh-add
+    ssh-add -l
+  }
+}
+Function p {
+  cd $env:USERPROFILE/Documents
+}
+Function ssho {
+  Param(
+    [string[]]$Pass
+  )
+ & ssh -t @Pass -- exec bin/onemux
 }
