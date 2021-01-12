67e2ca7dd8a0aef07f6d34542b49724561498258
ypcrts <32625281+ypcrts@users.noreply.github.com>
2021-01-12 16:54:13 +0000

    hostwise-incl

--- default/bash/interactive.sh
+++ default/bash/interactive.sh
@@ -1,3 +1,9 @@
+#
+# Assumptions: Bash-compatible interactive shell.
+# Ergo: Bashisms are authorized.
+# https://wiki.bash-hackers.org/scripting/bashchanges
+#
+
 #{{{1 functions
 #-------------------------------------------------------------------------------
 SOURCE_TRY () { [[ -f "$1" ]] && . "$1"; }
@@ -12,15 +18,33 @@ SOURCE_FIRST () {
 
 #{{{1 source base
 #-------------------------------------------------------------------------------
+# source this or error verbosel
 . ~/.config/bash/aliases.sh
-# XXX: TODO: change os-aliases to posix sh
+
+# os-aliases.bash might not exist
 SOURCE_TRY ~/.config/bash/os-aliases.bash
 
-#{{{1 includes hostwise
+#{{{1 hostwise includes
 #-------------------------------------------------------------------------------
-[[ -z "$HOSTNAME" ]] && HOSTNAME="$(hostname)"
-HOSTIDENT="$(echo -n "$HOSTNAME" | sed 's/\..*//')"
-SOURCE_TRY ~/.config/bash/hosts/"${HOSTIDENT}.sh"
-SOURCE_TRY ~/.config/bash/hosts/"${HOSTIDENT}.private.sh"
 SOURCE_TRY ~/.config/bash/hosts/local.private.sh
-unset HOSTIDENT
+hostwise_includes () {
+  local fqhost domain hostident
+  fqhost="$(uname -n)"
+
+  # TODO: impl domain, fqdn label pop til 2 labels, maybe
+  # TODO: STOP OVERENGINEERING EVERYTHING
+  # domain="${fqhost#*.}"
+  # [ -z "$domain" ] && domain="$(echo -n "$fqhost" | sed 's/^[^.]*[.]//')"
+  # SOURCE_TRY ~/.config/bash/domains/"${domain}.sh"
+
+  # XXX: tech debt - use full hostname||gtfo
+  hostident="${fqhost%%.*}"
+  [ -z "$hostident" ] && hostident="$(echo -n "$fqhost" | sed 's/^[.].*//')"
+  SOURCE_TRY ~/.config/bash/hosts/"${hostident}.sh"
+  SOURCE_TRY ~/.config/bash/hosts/"${hostident}.private.sh"
+
+  # The order matters. Let this be last.
+  SOURCE_TRY ~/.config/bash/domains/"${fqhost}.sh"
+}
+hostwise_includes
+unset -f hostwise_includes
