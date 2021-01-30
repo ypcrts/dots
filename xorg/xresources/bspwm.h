0b22efa60ca33c41f06edf506c298bf2ce631f0b
ypcrts <32625281+ypcrts@users.noreply.github.com>
2021-01-30 11:44:07 -0500

    bspwm xres

--- xorg/xresources/bspwm.h
+++ xorg/xresources/bspwm.h
@@ -1,6 +1,10 @@
 ! vim:fenc=utf-8:nu:ai:si:et:ts=4:sw=4:ft=xdefaults:
 
+#define SS_FONT_FAMILY DejaVu Sans
+#define FONT_FAMILY    Iosevka Term
+
 #include "common.h"
+#include "rofi.h"
 #include "color_w0ng.h"
 
 ! {{{1 Xautolock
@@ -12,28 +16,6 @@ Xautolock.notify:      15
 ! {{{1 SSH askpass
 ! https://wiki.archlinux.org/index.php/SSH_Keys#Calling_x11-ssh-askpass_with_ssh-add
 *askpass*background:                 #000000
-
-! {{{1 Rofi
-rofi.color-enabled:   true
-!                     bg,        fg,        bgalt,     hlbg,      hlfg
-rofi.color-normal:    #00000000, #FFffffff, #00000000, #CC000000, #FFffffff
-rofi.color-urgent:    #000000,   #ffffff,   #000000,   #333333,   #ffffff
-rofi.color-active:    #000000,   #ffffff,   #000000,   #333333,   #ffffff
-rofi.color-window:    #aa000000, #aa222222
-rofi.terminal:        urxvt
-rofi.location:        0
-rofi.padding:         30
-rofi.separator-style: solid
-rofi.threads:         0
-rofi.lines:           10
-rofi.fixed-num-lines: true
-rofi.eh:              2
-rofi.bw:              0
-rofi.hide-scrollbar:  true
-rofi.font:            Iosevka Term 10
-rofi.fullscreen:      false
-
-
 
 ! {{{1 Urxvt
 #define PURPLE_CURSOR #5E468C
