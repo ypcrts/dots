7cbecfc3dc33b45dfbe2ed4206061f4258a9a228
ypcrts <32625281+ypcrts@users.noreply.github.com>
2026-03-08 01:06:01 -0500

    2026

--- macos/aliases.bash
+++ macos/aliases.bash
@@ -1,8 +1,8 @@
 # vi: ft=sh sts=2 sw=2 sts=2 et
 alias firefoxp='/Applications/Firefox.app/Contents/MacOS/firefox  --no-remote -P'
 alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
-alias pbcopy='/usr/local/bin/reattach-to-user-namespace pbcopy'
-alias pbpaste='/usr/local/bin/reattach-to-user-namespace pbpaste'
+#alias pbcopy='/usr/local/bin/reattach-to-user-namespace pbcopy'
+#alias pbpaste='/usr/local/bin/reattach-to-user-namespace pbpaste'
 alias pbp='pbpaste'
 alias pbc='pbcopy'
 pidof () { ps -Acw | grep -iE $@ | awk '{print $1}'; } # this is kind of trash
